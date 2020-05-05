b75d44f6b67eff125873c145591a93cd635acca8
Nepcam <cameron.nepe@gmail.com>
2020-05-06 10:28:26 +1200

    Add project files.

---
+++ PracTest2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PracTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDrawPlanner = new System.Windows.Forms.Button();
+            this.textBoxNumOfHours = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(710, 625);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(586, 678);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(136, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(438, 678);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(136, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDrawPlanner
+            // 
+            this.btnDrawPlanner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDrawPlanner.Location = new System.Drawing.Point(289, 678);
+            this.btnDrawPlanner.Name = "btnDrawPlanner";
+            this.btnDrawPlanner.Size = new System.Drawing.Size(136, 23);
+            this.btnDrawPlanner.TabIndex = 3;
+            this.btnDrawPlanner.Text = "Draw Planner";
+            this.btnDrawPlanner.UseVisualStyleBackColor = true;
+            this.btnDrawPlanner.Click += new System.EventHandler(this.btnDrawPlanner_Click);
+            // 
+            // textBoxNumOfHours
+            // 
+            this.textBoxNumOfHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumOfHours.Location = new System.Drawing.Point(134, 679);
+            this.textBoxNumOfHours.Name = "textBoxNumOfHours";
+            this.textBoxNumOfHours.Size = new System.Drawing.Size(143, 22);
+            this.textBoxNumOfHours.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 682);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of Hours:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 735);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNumOfHours);
+            this.Controls.Add(this.btnDrawPlanner);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Week 5 Exercise 1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDrawPlanner;
+        private System.Windows.Forms.TextBox textBoxNumOfHours;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PracTest2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PracTest2
+{
+    public partial class Form1 : Form
+    {
+        //Name: Cameron Nepe
+        //Id: 1262199
+
+        //The day number that Saturday falls on
+        const int SATURDAY = 6;
+        //The number of days to display in the planner
+        const int NUM_DAYS = 7;
+        //The minimum number of hours to display
+        const int MIN_HOURS = 1;
+        //The maximum number of hours to display
+        const int MAX_HOURS = 24;
+        //the colour of a weekday appointment (a variable since Color datatype cannot be a const)
+        Color WEEK_DAY_COLOR = Color.White;
+        //the colour of a weekend appointment (a variable since Color datatype cannot be a const)
+        Color WEEK_END_COLOR = Color.LightBlue;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxNumOfHours.Clear();
+            pictureBoxDisplay.Refresh();
+            textBoxNumOfHours.Focus();
+        }
+
+        private void btnDrawPlanner_Click(object sender, EventArgs e)
+        {
+            //Where to draw the graphics
+            Graphics paper = pictureBoxDisplay.CreateGraphics();
+            //Brush to fill blocks
+            SolidBrush br = new SolidBrush(Color.White);
+            Pen pen = new Pen(Color.Black, 3);
+            //Declare variables
+            int numOfHours;
+            int numOfDays = 7;
+            //x position of appointment block
+            int x = 0;
+            //y position of appointment block
+            int y = 0;
+
+            try
+            {
+                //GET the number of hours
+                numOfHours = int.Parse(textBoxNumOfHours.Text);
+                //IF number of hours is valid THEN
+                if (numOfHours <= MAX_HOURS)
+                {
+                    //Calculate width of appointment
+                    int apptWidth = pictureBoxDisplay.Width / NUM_DAYS;
+                    //Calculate height of appointment
+                    int apptHeight = pictureBoxDisplay.Height / numOfHours;
+                    //FOR each hour to draw
+                    for (int row = 1; row <= numOfHours; row++)
+                    {
+                        //FOR each day to draw
+                        for (int col = 1; col <= numOfDays; col++)
+                        {
+                            //IF day is Saturday OR day is Sunday THEN
+                            if (col == SATURDAY || col == 7)
+                            {
+                                //Set colour to Light Blue
+                                br.Color = Color.LightBlue;
+                            }
+                            else
+                            {
+                                //Set colour to White
+                                br.Color = Color.White;
+                            }//ENDIF
+                             //Draw appointment at current x and y position
+                             paper.FillRectangle(br, x, y, apptWidth, apptHeight);
+                             paper.DrawRectangle(pen, x, y, apptWidth, apptHeight);
+                            //Shift x position to right by appointment width
+                            x+= apptWidth;                           
+                        }//ENDFOR
+                         //Shift y position down by appointment height
+                         y += apptHeight;
+                        //Shift x position to beginning of row
+                        x = 0;
+                    }//ENDFOR
+                }
+                else 
+                {
+                    //DISPLAY error message
+                    MessageBox.Show("Enter a number of hours please");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Display error message
+                MessageBox.Show(ex.Message);
+                //Clear number of hours textbox
+                textBoxNumOfHours.Clear();
+                pictureBoxDisplay.Refresh();
+                //Give focus to the number of hours textbox
+                textBoxNumOfHours.Focus();
+            }//End Try
+        }
+    }
+}
